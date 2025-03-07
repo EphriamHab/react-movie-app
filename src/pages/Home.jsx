c6b7e7f9907ee53fe16daa37df698e00216412c2
EphriamHab <ephremhabtamu2015@gmail.com>
2025-03-07 04:01:43 -0500

    Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,53 +1,72 @@
-import { useState } from "react";
+/* eslint-disable no-unused-vars */
+import { useState, useEffect } from "react";
 import MovieCard from "../components/MovieCard";
+import { getPopularMovies, searchMovies } from "../services/api";
+import "../css/Home.css";
 
 function Home() {
-    const [searchQuery, setSearchQuery] = useState("");
-    
-  const movies = [
-    {
-      id: 1,
-      title: "John Wick ",
-      release_date: "21/2/2003",
-      url: "https://www.google.com",
-    },
-    {
-      id: 2,
-      title: "Terminator",
-      release_date: "21/2/2003",
-      url: "https://www.google.com",
-    },
-    {
-      id: 3,
-      title: "The Matrix",
-      release_date: "21/2/2003",
-      url: "https://www.google.com",
-    },
-  ];
-    
-    
-    const handleSearch = (e) => { 
-        e.preventDefault()
-        console.log(searchQuery)
+  const [searchQuery, setSearchQuery] = useState("");
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-        setSearchQuery("")
+  useEffect(() => {
+    const loadPopularMovies = async () => {
+      try {
+        const popularMovies = await getPopularMovies();
+        setMovies(popularMovies);
+      } catch (error) {
+        console.error(error);
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    loadPopularMovies();
+  }, []);
+
+  const handleSearch = async(e) => {
+    e.preventDefault();
+    if (!searchQuery.trim) return;
+    if (loading) return;
+  
+    setLoading(true);
+
+    try {
+      const searchResults = await searchMovies(searchQuery);
+      setMovies(searchResults);
+    } catch (error) {
+      console.error(error);
+      setError(error);
+    } finally {
+      setLoading(false);
     }
-
-
+  };
 
   return (
-      <div className="home">
-          <form onSubmit={handleSearch} className="search-form">
-              <input type="text" value={searchQuery} onChange={(e)=>setSearchQuery(e.target.value)} placeholder="Search for a movie" className="search-input" />
-                <button type="submit" className="search-button">Search</button>
-          </form>
-              
-              
-      <div className="movies-grid">
-        {movies.map((movie) => (
-          <MovieCard key={movie.id} movie={movie} />
-        ))}
-      </div>
+    <div className="home">
+      <form onSubmit={handleSearch} className="search-form">
+        <input
+          type="text"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          placeholder="Search for a movie"
+          className="search-input"
+        />
+        <button type="submit" className="search-button">
+          Search
+        </button>
+      </form>
+      {error && <div className="error-message">{error}</div>}
+      {loading ? (
+        <div className="loading">Loading...</div>
+      ) : (
+        <div className="movies-grid">
+          {movies.map((movie) => (
+            <MovieCard key={movie.id} movie={movie} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
