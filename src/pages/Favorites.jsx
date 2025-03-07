614d02f0b477db27ebb80a7323e6df67084be408
EphriamHab <ephremhabtamu2015@gmail.com>
2025-03-07 00:53:51 -0500

    Favorites

---
+++ src/pages/Favorites.jsx
@@ -0,0 +1,13 @@
+
+
+
+function Favorites() {
+  return (
+      <div className="favorites-empty">
+          <h1>No favorites yet</h1>
+          <p>Click on the ❤️ button on a movie card to add it to your favorites</p>
+    </div>
+  );
+}
+
+export default Favorites;
