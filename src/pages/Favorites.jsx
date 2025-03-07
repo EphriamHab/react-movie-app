024decb74bbbd700c551a0076ae8da6e6dc96fcd
EphriamHab <ephremhabtamu2015@gmail.com>
2025-03-07 04:01:31 -0500

    Favorites

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,11 +1,27 @@
-
-
+import "../css/Favorites.css";
+import { useMovieContext } from "../contexts/MovieContext";
+import MovieCard from "../components/MovieCard";
 
 function Favorites() {
+  const { favorites } = useMovieContext();
+
+  if (favorites) {
+    return (
+      <div className="favorites">
+        <h2>Your Favorites</h2>
+        <div className="movies-grid">
+          {favorites.map((movie) => (
+            <MovieCard key={movie.id} movie={movie} />
+          ))}
+        </div>
+      </div>
+    );
+  }
+
   return (
-      <div className="favorites-empty">
-          <h1>No favorites yet</h1>
-          <p>Click on the ❤️ button on a movie card to add it to your favorites</p>
+    <div className="favorites-empty">
+      <h1>No favorites yet</h1>
+      <p>Click on the ❤️ button on a movie card to add it to your favorites</p>
     </div>
   );
 }
